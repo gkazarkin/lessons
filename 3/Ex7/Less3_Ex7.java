566e4847584237c660c4682d797b1b968fcc6e8f
Gleb Kazarkin <gkazarkin@sinergo.ru>
2019-03-12 15:45:02 +0700

    Less 3 Ex7

---
+++ 3/Ex7/Less3_Ex7.java
@@ -0,0 +1,38 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+
+public class Less3_Ex7 {
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("AutomationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\Users\\gkazarkin\\Downloads\\Auto\\Уроки\\AppiumTests\\apks\\org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    @After
+    public void tearDown() {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.quit();
+    }
+
+    @Test
+    public void AnyTest() {
+        //Просто запускается приложение, то есть мы прописали портретную ориентацию в @Before и @After
+    }
+}
